54ef10845022c9a0d20b9750ba809dc330088b5b
kkrentz <konrad.krentz@googlemail.com>
2014-11-13 10:17:37 -0800

    Deleted frame802154.c and most of frame802154.h

--- core/net/mac/frame802154.h
+++ core/net/mac/frame802154.h
@@ -114,41 +114,6 @@
 #define FRAME802154_5_BYTE_KEY_ID_MODE         (2)
 #define FRAME802154_9_BYTE_KEY_ID_MODE         (3)
 
-/**
- *    @brief  The IEEE 802.15.4 frame has a number of constant/fixed fields that
- *            can be counted to make frame construction and max payload
- *            calculations easier.
- *
- *            These include:
- *            1. FCF                  - 2 bytes       - Fixed
- *            2. Sequence number      - 1 byte        - Fixed
- *            3. Addressing fields    - 4 - 20 bytes  - Variable
- *            4. Aux security header  - 0 - 14 bytes  - Variable
- *            5. CRC                  - 2 bytes       - Fixed
-*/
-
-/**
- * \brief Defines the bitfields of the frame control field (FCF).
- */
-typedef struct {
-  uint8_t frame_type;        /**< 3 bit. Frame type field, see 802.15.4 */
-  uint8_t security_enabled;  /**< 1 bit. True if security is used in this frame */
-  uint8_t frame_pending;     /**< 1 bit. True if sender has more data to send */
-  uint8_t ack_required;      /**< 1 bit. Is an ack frame required? */
-  uint8_t panid_compression; /**< 1 bit. Is this a compressed header? */
-  /*   uint8_t reserved; */  /**< 3 bit. Unused bits */
-  uint8_t dest_addr_mode;    /**< 2 bit. Destination address mode, see 802.15.4 */
-  uint8_t frame_version;     /**< 2 bit. 802.15.4 frame version */
-  uint8_t src_addr_mode;     /**< 2 bit. Source address mode, see 802.15.4 */
-} frame802154_fcf_t;
-
-/** \brief 802.15.4 security control bitfield.  See section 7.6.2.2.1 in 802.15.4 specification */
-typedef struct {
-  uint8_t  security_level; /**< 3 bit. security level      */
-  uint8_t  key_id_mode;    /**< 2 bit. Key identifier mode */
-  uint8_t  reserved;       /**< 3 bit. Reserved bits       */
-} frame802154_scf_t;
-
 typedef union {
   uint32_t u32;
   uint16_t u16[2];
@@ -159,39 +124,6 @@ typedef union {
   uint16_t u16[4];
   uint8_t u8[8];
 } frame802154_key_source_t;
-
-/** \brief 802.15.4 Aux security header */
-typedef struct {
-  frame802154_scf_t security_control;        /**< Security control bitfield */
-  frame802154_frame_counter_t frame_counter; /**< Frame counter, used for security */
-  frame802154_key_source_t key_source;       /**< Key Source subfield */
-  uint8_t key_index;                         /**< Key Index subfield */
-} frame802154_aux_hdr_t;
-
-/** \brief Parameters used by the frame802154_create() function.  These
- *  parameters are used in the 802.15.4 frame header.  See the 802.15.4
- *  specification for details.
- */
-typedef struct {
-  /* The fields dest_addr and src_addr must come first to ensure they are aligned to the
-   * CPU word size. Needed as they are accessed directly as linkaddr_t*. Note we cannot use
-   * the type linkaddr_t directly here, as we always need 8 bytes, not LINKADDR_SIZE bytes. */
-  uint8_t dest_addr[8];           /**< Destination address */
-  uint8_t src_addr[8];            /**< Source address */
-  frame802154_fcf_t fcf;          /**< Frame control field  */
-  uint8_t seq;                    /**< Sequence number */
-  uint16_t dest_pid;              /**< Destination PAN ID */
-  uint16_t src_pid;               /**< Source PAN ID */
-  frame802154_aux_hdr_t aux_hdr;  /**< Aux security header */
-  uint8_t *payload;               /**< Pointer to 802.15.4 payload */
-  int payload_len;                /**< Length of payload field */
-} frame802154_t;
-
-/* Prototypes */
-
-int frame802154_hdrlen(frame802154_t *p);
-int frame802154_create(frame802154_t *p, uint8_t *buf);
-int frame802154_parse(uint8_t *data, int length, frame802154_t *pf);
 
 /** @} */
 #endif /* FRAME_802154_H */
