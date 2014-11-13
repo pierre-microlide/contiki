faeb24bc0069daa1599b87c46b4036cf0a8953d2
kkrentz <konrad.krentz@googlemail.com>
2014-11-13 09:44:11 -0800

    sicslowmac: Removed dependency on frame802154.c

--- core/net/mac/sicslowmac/sicslowmac.c
+++ core/net/mac/sicslowmac/sicslowmac.c
@@ -45,11 +45,9 @@
 
 #include <string.h>
 #include "net/mac/sicslowmac/sicslowmac.h"
-#include "net/mac/frame802154.h"
 #include "net/packetbuf.h"
 #include "net/queuebuf.h"
 #include "net/netstack.h"
-#include "lib/random.h"
 
 #define DEBUG 0
 
@@ -62,117 +60,23 @@
 #define PRINTADDR(addr)
 #endif
 
-/**  \brief The sequence number (0x00 - 0xff) added to the transmitted
- *   data or MAC command frame. The default is a random value within
- *   the range.
- */
-static uint8_t mac_dsn;
-
-/**  \brief The 16-bit identifier of the PAN on which the device is
- *   sending to.  If this value is 0xffff, the device is not
- *   associated.
- */
-static uint16_t mac_dst_pan_id = IEEE802154_PANID;
-
-/**  \brief The 16-bit identifier of the PAN on which the device is
- *   operating.  If this value is 0xffff, the device is not
- *   associated.
- */
-static uint16_t mac_src_pan_id = IEEE802154_PANID;
-
-/*---------------------------------------------------------------------------*/
-static int
-is_broadcast_addr(uint8_t mode, uint8_t *addr)
-{
-  int i = mode == FRAME802154_SHORTADDRMODE ? 2 : 8;
-  while(i-- > 0) {
-    if(addr[i] != 0xff) {
-      return 0;
-    }
-  }
-  return 1;
-}
 /*---------------------------------------------------------------------------*/
 static void
 send_packet(mac_callback_t sent, void *ptr)
 {
-  frame802154_t params;
-  uint8_t len;
-
-  /* init to zeros */
-  memset(&params, 0, sizeof(params));
-
-  /* Build the FCF. */
-  params.fcf.frame_type = FRAME802154_DATAFRAME;
-  params.fcf.security_enabled = 0;
-  params.fcf.frame_pending = 0;
-  params.fcf.ack_required = packetbuf_attr(PACKETBUF_ATTR_RELIABLE);
-  params.fcf.panid_compression = 0;
-
-  /* Insert IEEE 802.15.4 (2003) version bit. */
-  params.fcf.frame_version = FRAME802154_IEEE802154_2003;
-
-  /* Increment and set the data sequence number. */
-  params.seq = mac_dsn++;
-
-  /* Complete the addressing fields. */
-  /**
-     \todo For phase 1 the addresses are all long. We'll need a mechanism
-     in the rime attributes to tell the mac to use long or short for phase 2.
-  */
-  params.fcf.src_addr_mode = FRAME802154_LONGADDRMODE;
-  params.dest_pid = mac_dst_pan_id;
-
-  if(packetbuf_holds_broadcast()) {
-    /* Broadcast requires short address mode. */
-    params.fcf.dest_addr_mode = FRAME802154_SHORTADDRMODE;
-    params.dest_addr[0] = 0xFF;
-    params.dest_addr[1] = 0xFF;
-
-  } else {
-    linkaddr_copy((linkaddr_t *)&params.dest_addr,
-                  packetbuf_addr(PACKETBUF_ADDR_RECEIVER));
-    params.fcf.dest_addr_mode = FRAME802154_LONGADDRMODE;
-  }
-
-  /* Set the source PAN ID to the global variable. */
-  params.src_pid = mac_src_pan_id;
-
-  /*
-   * Set up the source address using only the long address mode for
-   * phase 1.
-   */
-#if NETSTACK_CONF_BRIDGE_MODE
-  linkaddr_copy((linkaddr_t *)&params.src_addr,packetbuf_addr(PACKETBUF_ADDR_SENDER));
-#else
-  linkaddr_copy((linkaddr_t *)&params.src_addr, &linkaddr_node_addr);
+  int ret;
+  
+#if !NETSTACK_CONF_BRIDGE_MODE
+  packetbuf_set_addr(PACKETBUF_ADDR_SENDER, &linkaddr_null);
 #endif
-
-  params.payload = packetbuf_dataptr();
-  params.payload_len = packetbuf_datalen();
-  len = frame802154_hdrlen(&params);
-  if(packetbuf_hdralloc(len)) {
-    int ret;
-    frame802154_create(&params, packetbuf_hdrptr());
-
-    PRINTF("6MAC-UT: %2X", params.fcf.frame_type);
-    PRINTADDR(params.dest_addr);
-    PRINTF("%u %u (%u)\n", len, packetbuf_datalen(), packetbuf_totlen());
-
-    ret = NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen());
-    if(sent) {
-      switch(ret) {
-      case RADIO_TX_OK:
-        sent(ptr, MAC_TX_OK, 1);
-        break;
-      case RADIO_TX_ERR:
-        sent(ptr, MAC_TX_ERR, 1);
-        break;
-      }
-    }
-  } else {
-    PRINTF("6MAC-UT: too large header: %u\n", len);
+  
+  if(NETSTACK_FRAMER.create_and_secure() < 0) {
+    PRINTF("6MAC: Frame creation failed\n");
+    return;
   }
+  
+  ret = NETSTACK_RADIO.send(packetbuf_hdrptr(), packetbuf_totlen());
+  mac_call_sent_callback(sent, ptr, ret, 1);
 }
 /*---------------------------------------------------------------------------*/
 void
@@ -187,42 +91,25 @@ send_list(mac_callback_t sent, void *ptr, struct rdc_buf_list *buf_list)
 static void
 input_packet(void)
 {
-  frame802154_t frame;
-  int len;
-
-  len = packetbuf_datalen();
-
-  if(frame802154_parse(packetbuf_dataptr(), len, &frame) &&
-     packetbuf_hdrreduce(len - frame.payload_len)) {
-    if(frame.fcf.dest_addr_mode) {
-      if(frame.dest_pid != mac_src_pan_id &&
-         frame.dest_pid != FRAME802154_BROADCASTPANDID) {
-        /* Not broadcast or for our PAN */
-        PRINTF("6MAC: for another pan %u\n", frame.dest_pid);
-        return;
-      }
-      if(!is_broadcast_addr(frame.fcf.dest_addr_mode, frame.dest_addr)) {
-        packetbuf_set_addr(PACKETBUF_ADDR_RECEIVER, (linkaddr_t *)&frame.dest_addr);
-#if !NETSTACK_CONF_BRIDGE_MODE
-        if(!linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),
-                         &linkaddr_node_addr)) {
-          /* Not for this node */
-          PRINTF("6MAC: not for us\n");
-          return;
-        }
-#endif
-      }
-    }
-    packetbuf_set_addr(PACKETBUF_ADDR_SENDER, (linkaddr_t *)&frame.src_addr);
-
-    PRINTF("6MAC-IN: %2X", frame.fcf.frame_type);
-    PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_SENDER));
-    PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_RECEIVER));
-    PRINTF("%u\n", packetbuf_datalen());
-    NETSTACK_MAC.input();
-  } else {
-    PRINTF("6MAC: failed to parse hdr\n");
+  if(NETSTACK_FRAMER.parse() < 0) {
+    PRINTF("6MAC: Frame parsing failed\n");
+    return;
   }
+  
+  if(!packetbuf_holds_broadcast()
+#if !NETSTACK_CONF_BRIDGE_MODE
+      && !linkaddr_cmp(packetbuf_addr(PACKETBUF_ADDR_RECEIVER),
+          &linkaddr_node_addr)
+#endif
+      ) {
+    PRINTF("6MAC: not for us\n");
+  }
+  
+  PRINTF("6MAC-IN: %2X", packetbuf_attr(PACKETBUF_ATTR_FRAME_TYPE));
+  PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_SENDER));
+  PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_RECEIVER));
+  PRINTF("%u\n", packetbuf_datalen());
+  NETSTACK_MAC.input();
 }
 /*---------------------------------------------------------------------------*/
 static int
@@ -244,8 +131,6 @@ off(int keep_radio_on)
 static void
 init(void)
 {
-  mac_dsn = random_rand() % 256;
-
   NETSTACK_RADIO.on();
 }
 /*---------------------------------------------------------------------------*/
